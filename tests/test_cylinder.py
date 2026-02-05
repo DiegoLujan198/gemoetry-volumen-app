cd12d35cd47c38ac8ed9db95dee9bd88811dd1eb
DiegoLujan198 <diegomarcelo.gonzalez@udem.edu>
2026-02-05 13:53:07 -0600

    Develop, test, and validate test_cylinder.py

--- tests/test_cylinder.py
+++ tests/test_cylinder.py
@@ -4,7 +4,7 @@ from geometry.cylinder import volume_cylinder
 
 def test_volume_cylinder_valid():
     # Siguiendo el ejemplo que tenemos en el cylinder.py
-    assert volume_cylinder(3.0,5.0) == pytest.approx(141.372, rel = 1e-6)
+    assert volume_cylinder(3.0,5.0) == pytest.approx(141.3716694115407, rel = 1e-6)
 
 def test_volume_cylinder_negative():
     #Esta vez vamos a ver como se comporta si el radio o la altura sea negativo
